12cd2cad542e7e88ea86dd6f576a49897c6f7aef
Joe Marley <joe.d.marley@seagate.com>
2019-01-07 14:37:58 -0700

    Add quotes around path to pet.cpp

--- src/src.cmake
+++ src/src.cmake
@@ -1,8 +1,7 @@
 # Set current directory for adding sources
 set (HERE ${CMAKE_CURRENT_LIST_DIR})
 
-target_sources("${PROJECT_NAME}"
+target_sources( "${PROJECT_NAME}"
     PRIVATE
-    ${HERE}/pet.cpp
+    "${HERE}/pet.cpp"
     )
-
