6cae6ca3f9338cbc62014ee49deb1f333529c3ec
Joe Marley <joe.d.marley@seagate.com>
2019-01-07 14:35:14 -0700

    Add ifdef __linux__ to deal with GCC-specific pragmas.

--- inc/include_pybind11.h
+++ inc/include_pybind11.h
@@ -1,7 +1,11 @@
 #pragma once
 // pybind11 has some warnings that this project checks.
 // Suppress those warnings for the pybind11 headers.
+#ifdef __linux__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weffc++"
+#endif // __linux__
 #include "pybind11/pybind11.h"
+#ifdef __linux__
 #pragma GCC diagnostic pop
+#endif // __linux__
